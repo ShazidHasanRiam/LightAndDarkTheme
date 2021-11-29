12e079869512df6453dda43e450d22011319f9fe
shazidhasanriam <shazid.riam@northsouth.edu>
2021-11-29 18:48:06 +0600

    Initial Final Update

--- script.js
+++ script.js
@@ -14,18 +14,49 @@ function darkMode() {
   toggleIcon.children[0].textContent = "Dark Mode";
   toggleIcon.children[1].classList.remove("fa-sun");
   toggleIcon.children[1].classList.add("fa-moon");
+  image1.src = "Images/undraw_proud_coder_dark.svg";
+  image2.src = "Images/undraw_feeling_proud_dark.svg";
+  image3.src = "Images/undraw_conceptual_idea_dark.svg";
 }
+
+//Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  // console.log(toggleIcon.children);
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.remove("fa-moon");
+  toggleIcon.children[1].classList.add("fa-sun");
+  image1.src = "Images/undraw_proud_coder_light.svg";
+  image2.src = "Images/undraw_feeling_proud_light.svg";
+  image3.src = "Images/undraw_conceptual_idea_light.svg";
+}
+
 // Switch Theme
 function switchTheme(event) {
   //   console.log(event.target.checked);
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
     darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
     lightMode();
   }
 }
 
 // Event Listener
 toggleSwitch.addEventListener("change", switchTheme);
+
+//Check Local Storage For Theme
+const currentTheme = localStorage.getItem("theme");
+// console.log(currentTheme);
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
